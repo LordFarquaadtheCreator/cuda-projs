971fe1e751ba92882b13c288603f070fd5cf15f7
jaynopponep <noppone.p@gmail.com>
2024-11-21 12:11:24 -0500

    added calc_time to calculate time within haversine_library.cpp

--- proj2/haversine_library.cpp
+++ proj2/haversine_library.cpp
@@ -6,7 +6,7 @@
 
 void run_kernel(int size, const double *x1,const double *y1, const double *x2,const double *y2, double *dist);
 
-float calc_time(char *msg,timeval t0, timeval t1)
+float calc_time(const char *msg,timeval t0, timeval t1)
 {
  	long d = t1.tv_sec*1000000+t1.tv_usec - t0.tv_sec * 1000000-t0.tv_usec;
  	float t=(float)d/1000;
@@ -35,7 +35,11 @@ void haversine_distance(int size,pybind11::array_t<double> x1_v,pybind11::array_
   assert(y2_v.request().ndim==1);
   assert(dist_v.request().ndim==1);
 
+  timeval s0, s1, s2, s3;
+
   double *d_x1,*d_y1,*d_x2,*d_y2,*d_dist;
+  // cudaMalloc below allocates memory onto GPU from host
+  gettimeofday(&s0, NULL);
   HANDLE_ERROR( cudaMalloc(&d_x1, size * sizeof(double)) );
   HANDLE_ERROR( cudaMalloc(&d_y1, size * sizeof(double)) );
   HANDLE_ERROR( cudaMalloc(&d_x2, size * sizeof(double)) );
@@ -47,17 +51,23 @@ void haversine_distance(int size,pybind11::array_t<double> x1_v,pybind11::array_
   double* h_x2 = reinterpret_cast<double*>(x2_v.request().ptr);
   double* h_y2 = reinterpret_cast<double*>(y2_v.request().ptr);
   double* h_dist = reinterpret_cast<double*>(dist_v.request().ptr);
-
+  // copy (cpy) data from host to device (cudaMemcpyHostToDevice) 
   HANDLE_ERROR( cudaMemcpy(d_x1, h_x1, size * sizeof(double), cudaMemcpyHostToDevice) );
   HANDLE_ERROR( cudaMemcpy(d_y1, h_y1, size * sizeof(double), cudaMemcpyHostToDevice) );
   HANDLE_ERROR( cudaMemcpy(d_x2, h_x2, size * sizeof(double), cudaMemcpyHostToDevice) );
   HANDLE_ERROR( cudaMemcpy(d_y2, h_y2, size * sizeof(double), cudaMemcpyHostToDevice) );
-
+  gettimeofday(&s1, NULL);
+  calc_time("transferring from CPU to GPU\n", s0, s1);
   //printf("before\n");
+  // run the kernel
   run_kernel(size,d_x1,d_y1,d_x2,d_y2,d_dist);
+  gettimeofday(&s2, NULL);
+  calc_time("running kernel\n", s1, s2);
   //printf("after\n");
-
+  //transfer data back from device to host or gpu to cpu (cudaMemcpyDeviceToHost)
   HANDLE_ERROR( cudaMemcpy(h_dist, d_dist, size * sizeof(double), cudaMemcpyDeviceToHost) );
+  gettimeofday(&s3, NULL);
+  calc_time("GPU to CPU\n", s2, s3);
 
   HANDLE_ERROR( cudaFree(d_x1) );
   HANDLE_ERROR( cudaFree(d_y1) );
