b1d05a2de024b02b8b1654ec3837b75c355f8bfa
joewadk <jjkabir000@citymail.cuny.edu>
2024-11-25 14:17:38 -0500

    getting started on q4

---
+++ proj2/dataframe_test.py
@@ -0,0 +1,32 @@
+import cudf
+import numpy as np
+from haversine_library import haversine_library#<--- this doesnt work
+
+NYC_BOUNDING_BOX = [-74.15, 40.5774, -73.7004, 40.9176] #see dosc for more info
+
+def filter_nyc(df): 
+    return df[
+        (df['Start_Lon'] >= NYC_BOUNDING_BOX[0]) & (df['Start_Lon'] <= NYC_BOUNDING_BOX[2]) &
+        (df['Start_Lat'] >= NYC_BOUNDING_BOX[1]) & (df['Start_Lat'] <= NYC_BOUNDING_BOX[3]) &
+        (df['End_Lon'] >= NYC_BOUNDING_BOX[0]) & (df['End_Lon'] <= NYC_BOUNDING_BOX[2]) &
+        (df['End_Lat'] >= NYC_BOUNDING_BOX[1]) & (df['End_Lat'] <= NYC_BOUNDING_BOX[3])
+    ]
+
+#the loop
+file_paths = [f'tlcdata/yellow_tripdata_2009-{str(i).zfill(2)}.parquet' for i in range(1, 13)]
+dfs = []
+for file_path in file_paths:
+    for chunk in cudf.read_parquet(file_path, chunksize=100000): #this might be a cause of error. need to look more into it
+        df_filtered = filter_nyc(chunk)
+        dfs.append(df_filtered)
+
+#one big datagrame
+combined_df = cudf.concat(dfs, ignore_index=True)
+start_lon = combined_df['Start_Lon'].to_numpy()
+start_lat = combined_df['Start_Lat'].to_numpy()
+end_lon = combined_df['End_Lon'].to_numpy()
+end_lat = combined_df['End_Lat'].to_numpy()
+
+#this kinda assumes it works. dunno if it does
+distances = haversine_library.haversine_distance(start_lon, start_lat, end_lon, end_lat)
+#i think i have to add the rest of the distances to a dataframe.
