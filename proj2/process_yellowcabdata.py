8c8d81531ee2410bed5b47e1ccba5400653bb2d1
jaynopponep <noppone.p@gmail.com>
2024-12-07 16:48:57 -0500

    completed step4

--- proj2/process_yellowcabdata.py
+++ proj2/process_yellowcabdata.py
@@ -1,5 +1,10 @@
 import os
+import sys
 import pandas as pd
+import numpy as np
+sys.path.append('./build')
+#import cudf
+import haversine_library
 
 dir = "yellowcab_2009"
 desired_cols = ["Start_Lon", "Start_Lat", "End_Lon", "End_Lat"]
@@ -21,5 +26,13 @@ for file_name in os.listdir(dir):
     inbound_data = data[inbounds]
     main_df = pd.concat([main_df, data], ignore_index=True)
 # at this point all DFs are combined
-df_to_numpy = main_df.to_numpy()
-print(df_to_numpy.shape)
+#df_to_numpy = main_df.to_numpy()
+#print(df_to_numpy.shape)
+x1=main_df['Start_Lon'].to_numpy()
+y1=main_df['Start_Lat'].to_numpy()
+x2=main_df['End_Lon'].to_numpy()
+y2=main_df['End_Lat'].to_numpy()
+size=len(x1)
+dist=np.zeros(size)
+haversine_library.haversine_distance(size,x1,y1,x2,y2,dist)
+print(dist)
