89117bbf81a0cd37c0224ca1ac8fbc2fbb867bb4
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2024-12-08 12:55:20 -0500

    making code prettier

--- proj2/process_yellowcabdata.py
+++ proj2/process_yellowcabdata.py
@@ -1,22 +1,20 @@
 import os
 import sys
-import pandas as pd
 import numpy as np
+import cudf as cu
 sys.path.append('./build')
-#import cudf
 import haversine_library
 
-dir = "yellowcab_2009"
+DIR = "/data/csc59866_f24/tlcdata/"
 desired_cols = ["Start_Lon", "Start_Lat", "End_Lon", "End_Lat"]
-main_df = pd.DataFrame() # init df that will have all 12 months
-# note that both Start_Lon and End_Lon must lie between the bounding range of west and east: -74.15, -73.7004
-# Start_Lat and End_Lat must lie between bounding range of north and south: 40.5774, 40.9176
-for file_name in os.listdir(dir):
-    file_path = os.path.join(dir, file_name)
-    data = pd.read_parquet(file_path)
+main_df = cu.DataFrame()
+
+for file_name in os.listdir(DIR):
+    file_path = os.path.join(DIR, file_name)
+    data = cu.read_parquet(file_path)
 
     data = data[desired_cols]
-    # conditions to ensure coords are inbounds
+    
     inbounds = (
             (data["Start_Lon"] >= -74.15) & (data["Start_Lon"] <= -73.7004) &
             (data["End_Lon"] >= -74.15) & (data["End_Lon"] <= -73.7004) &
@@ -24,10 +22,8 @@ for file_name in os.listdir(dir):
             (data["End_Lat"] >= 40.5774) & (data["End_Lat"] <= 40.9176)
             )
     inbound_data = data[inbounds]
-    main_df = pd.concat([main_df, data], ignore_index=True)
-# at this point all DFs are combined
-#df_to_numpy = main_df.to_numpy()
-#print(df_to_numpy.shape)
+    main_df = cu.concat([main_df, data], ignore_index=True)
+
 x1=main_df['Start_Lon'].to_numpy()
 y1=main_df['Start_Lat'].to_numpy()
 x2=main_df['End_Lon'].to_numpy()
